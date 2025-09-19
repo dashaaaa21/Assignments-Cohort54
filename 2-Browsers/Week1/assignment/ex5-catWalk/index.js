42f0f57b54bce0f31d6c78683ca7e3b94882ba90
Dasha <daryna2003tk@gmail.com>
2025-09-19 19:02:19 +0200

    Modified assignment

--- 2-Browsers/Week1/assignment/ex5-catWalk/index.js
+++ 2-Browsers/Week1/assignment/ex5-catWalk/index.js
@@ -22,32 +22,50 @@ Full description at: https://github.com/HackYourFuture/Assignments/tree/main/2-B
 -----------------------------------------------------------------------------*/
 function catWalk() {
   const img = document.querySelector('img');
+  img.style.position = 'absolute';
   img.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
   img.style.left = '0px';
   let position = 0;
-  let danced = false;
+  let isDancing = false;
+  let dancedThisLap = false;
+  let timer = null;
 
   function step() {
+    if (isDancing) return;
     position = position +10;
     img.style.left = position + 'px';
 
-    const middle = (window.innerWidth - img.width) / 2;
-    const end = window.innerWidth - img.width;
+    const middle = (window.innerWidth - img.offsetWidth) / 2;
+    const end = window.innerWidth - img.offsetWidth;
 
-    if (!danced && position >= middle) {
-      danced = true;
+    if (!dancedThisLap && position >= middle) {
+      isDancing = true;
+      dancedThisLap = true;
+      clearInterval(timer);
       img.src = 'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
       setTimeout(() => {
         img.src = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
-      }, 5000);
+      isDancing = false;
+      timer = setInterval(step, 50);
+        }, 5000);
     }
     if (position >= end) {
       position = 0;
-      danced = false;
+      img.style.left = '0px';
+      dancedThisLap = false;
     }
   }
 
-  setInterval(step, 50);
+  function startWhenReady() {
+    if (img.complete && img.naturalWidth > 0) {
+      timer = setInterval(step, 50);
+    } else {
+      img.addEventListener('load', () => {
+        timer = setInterval(step, 50);
+      }, { once: true });
+    }
+  }
+  startWhenReady();
 }
 
 window.addEventListener('DOMContentLoaded', catWalk);
